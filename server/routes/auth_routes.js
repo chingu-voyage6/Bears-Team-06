18c284b955f16ddb58dfa955bf6e25ccea6ceb71
alexluong <aluong@crimson.ua.edu>
2018-07-10 19:33:24 -0500

    Sign up pseudo code

--- server/routes/auth_routes.js
+++ server/routes/auth_routes.js
@@ -9,55 +9,68 @@ function tokenForUser(user) {
   return jwt.encode({ sub: user, iat: timestamp }, 'bears');
 }
 
-
-router.get('/google', passport.authenticate('google', { scope: 'profile email' }));
-router.get('/google/redirect', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
-	res.json({
-	  userId: req.user._id,
-  	  username: req.user.name,
-  	  email: req.user.email,
-  	  token: tokenForUser(req.user._id)
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: 'profile email' }),
+);
+router.get(
+  '/google/redirect',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.json({
+      userId: req.user._id,
+      username: req.user.name,
+      email: req.user.email,
+      token: tokenForUser(req.user._id),
     });
-});
+  },
+);
 
+router.get(
+  '/facebook',
+  passport.authenticate('facebook', { scope: ['email', 'public_profile'] }),
+);
+router.get(
+  '/facebook/redirect',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.json({
+      userId: req.user._id,
+      username: req.user.name,
+      email: req.user.email,
+      token: tokenForUser(req.user._id),
+    });
+  },
+);
 
-router.get('/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile'] }));
-router.get('/facebook/redirect', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
-        res.json({
-    		userId: req.user._id,
-    		username: req.user.name,
-			email: req.user.email,
-			token: tokenForUser(req.user._id)
-    	});
-});
-
-
-
-router.post("/login",passport.authenticate('login') ,function(err, user, info){
-	// console.log(req.user);
-	res.json({
-		userId: req.user._id,
-		username: req.user.name,
-		email: req.user.email,
-		token: tokenForUser(req.user._id)
-	});
+router.post('/login', passport.authenticate('login'), function(
+  err,
+  user,
+  info,
+) {
+  // console.log(req.user);
+  res.json({
+    userId: req.user._id,
+    username: req.user.name,
+    email: req.user.email,
+    token: tokenForUser(req.user._id),
+  });
 });
 //signup with email
-router.post("/signup",passport.authenticate('signup'), function(req,res){
-	console.log(req.body);
-	const newUser = req.body;
-	User.findOne(newUser,newUser.password,(err,user)=>{
-		if (err){ return res.json(err.message); }
-		res.json({
-			userId: req.user._id,
-			username: req.user.email,
-			email: req.user.email,
-			token: tokenForUser(req.user._id)
-		});
+router.post('/signup', function(req, res) {
+  console.log(req.body);
+  res.status(200).send({ message: 'data received' });
+  // const newUser = req.body;
+  // User.findOne(newUser,newUser.password,(err,user)=>{
+  // 	if (err){ return res.json(err.message); }
+  // 	res.json({
+  // 		userId: req.user._id,
+  // 		username: req.user.email,
+  // 		email: req.user.email,
+  // 		token: tokenForUser(req.user._id)
+  // 	});
 
-	});
+  // });
 });
-
-
 
 module.exports = router;
