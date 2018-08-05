b597ba4c2da64368d55f26e00e01b6d690f9092e
Merline Raymond <raymondmerline@gmail.com>
2018-08-05 17:28:08 -0400

    new route

--- server/routes/goal_routes.js
+++ server/routes/goal_routes.js
@@ -3,6 +3,7 @@ const Goal = require('../models/goal');
 const User = require('../models/user');
 const passport = require('passport');
 const jwt = require('jwt-simple');
+const moment = require('moment');
 
 //post goal
 router.post('/submit', function(req, res) {
@@ -22,6 +23,7 @@ router.post('/submit', function(req, res) {
      userGoal.goal = goal;
      userGoal.bounty_amount = bounty_amount;
      userGoal.non_profit = non_profit;
+     userGoal.last_check_in = moment().format("YYYY-MM-DD");
      userGoal.due_date = due_date;
     // save the goal
     userGoal.save(function(err, savedGoal) {
@@ -34,7 +36,7 @@ router.post('/submit', function(req, res) {
  });
 
 //get all goals
-router.get('/', function(req, res) {
+router.post('/check', function(req, res) {
   const user_id = req.user._id;
      // if no user set
      if(!user_id){
@@ -58,5 +60,55 @@ router.get('/', function(req, res) {
     });
 });
 
+//daily checkin for goal
+router.post('/checkin', function(req, res) {
+  const user_id = req.user._id;
+  const goal = req.body.goal;
+
+     // if no user set
+     if(!user_id){
+       res.status(500).send({ message: 'no user set' });
+       return;
+     }
+
+     // if no goal set
+     if(!goal){
+       res.status(500).send({ message: 'no goal set' });
+       return;
+     }
+
+     //find the goal for one user
+     Goal.findOne({user_id:user_id, goal:goal} , function(err, goals){
+       if (err) {
+          res.status(500).send({ message: 'error getting goals' });
+          return;
+        }
+
+        var yesterday     = moment().subtract(1, 'days').format("YYYY-MM-DD");
+        var today         = moment().format("YYYY-MM-DD");
+        var last_check_in = moment(goals.last_check_in).format('YYYY-MM-DD');
+        var due_date      = moment(goals.due_date).format('YYYY-MM-DD');
+
+        //was last checkin yesterday and not today(already checked in)
+        if(last_check_in != yesterday && last_check_in != today){
+          res.status(500).send({ message: 'O dear! You missed a check in on your goal. You have failed.' });
+          return;
+        }
+
+        //is today equal to due date
+        if(today == due_date){
+          goals.last_check_in = moment().format("YYYY-MM-DD");
+          res.status(200).send({ message: 'Congratulations! You have completed your goal.' });
+          return;
+        }
+
+        //if last checkin was yesterday
+        if(last_check_in == yesterday){
+          goals.last_check_in = moment().format("YYYY-MM-DD");
+          res.status(200).send({ message: 'Successfully checked in for today. Keep up the great work!' });
+          return;
+        }
+    });
+});
 
 module.exports = router;
